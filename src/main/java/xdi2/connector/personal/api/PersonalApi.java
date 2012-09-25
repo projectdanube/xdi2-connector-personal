343782b8bc11bba15d75d42e426a402a0aba9e21
peacekeeper <markus.sabadello@gmail.com>
2012-09-26 01:58:23 +0200

    adding oauth logo, and some updates to logging and error handling in PersonalApi

--- src/main/java/xdi2/connector/personal/api/PersonalApi.java
+++ src/main/java/xdi2/connector/personal/api/PersonalApi.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
@@ -45,7 +47,7 @@ public class PersonalApi {
 
 		this.appId = null;
 		this.appSecret = null;
-		
+
 		this.httpClient = new DefaultHttpClient();
 	}
 
@@ -64,57 +66,54 @@ public class PersonalApi {
 		String url = "https://api-sandbox.personal.com/oauth/authorize?client_id="+client_id+"&response_type=code&redirect_uri="+req.getRequestURL().toString()+"&scope="+this.scope+"&update="+this.update;
 
 		resp.setContentType("text/plain");
-		
+
 		resp.sendRedirect(url);
 	}
-	
-	public StringBuffer postit(String code,String reqURL){
+
+	public StringBuffer postit(String code,String reqURL) throws IOException{
 		BufferedReader rd = null;
 		StringBuffer sb = new StringBuffer();
 
-    	try {
-            String postURL = "https://api-sandbox.personal.com/oauth/access_token";
-            // Construct data
-            String data = URLEncoder.encode("grant_type", "UTF-8") + "=" + URLEncoder.encode("authorization_code", "UTF-8");
-            data += "&" + URLEncoder.encode("code", "UTF-8") + "=" + URLEncoder.encode(code, "UTF-8");
-            data += "&" + URLEncoder.encode("client_id", "UTF-8") + "=" + URLEncoder.encode(this.appId, "UTF-8");
-            data += "&" + URLEncoder.encode("client_secret", "UTF-8") + "=" + URLEncoder.encode(this.appSecret, "UTF-8");
-            data += "&" + URLEncoder.encode("redirect_uri", "UTF-8") + "=" + URLEncoder.encode(reqURL, "UTF-8");
-            // Send data
-            URL url = new URL(postURL);
-            URLConnection conn = url.openConnection();
-            conn.setDoOutput(true);
-    		conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
-    		OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-            wr.write(data);
-            wr.flush();
+		String postURL = "https://api-sandbox.personal.com/oauth/access_token";
+		// Construct data
+		String data = URLEncoder.encode("grant_type", "UTF-8") + "=" + URLEncoder.encode("authorization_code", "UTF-8");
+		data += "&" + URLEncoder.encode("code", "UTF-8") + "=" + URLEncoder.encode(code, "UTF-8");
+		data += "&" + URLEncoder.encode("client_id", "UTF-8") + "=" + URLEncoder.encode(this.appId, "UTF-8");
+		data += "&" + URLEncoder.encode("client_secret", "UTF-8") + "=" + URLEncoder.encode(this.appSecret, "UTF-8");
+		data += "&" + URLEncoder.encode("redirect_uri", "UTF-8") + "=" + URLEncoder.encode(reqURL, "UTF-8");
+		// Send data
+		URL url = new URL(postURL);
+		URLConnection conn = url.openConnection();
+		conn.setDoOutput(true);
+		conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+		OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+		wr.write(data);
+		wr.flush();
 
-            // Get the response
-            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line;
-			while ((line = rd.readLine()) != null)
-			{
-				sb.append(line);
-			}
-            wr.close();
-            rd.close();
-        } catch (Exception e) {
-        	System.out.println("Exception: "+e);
-        }
-    	return sb;
-    }
+		// Get the response
+		rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		String line;
+		while ((line = rd.readLine()) != null)
+		{
+			sb.append(line);
+		}
+		wr.close();
+		rd.close();
+
+		return sb;
+	}
 
 	public String exchangeCodeForAccessToken(HttpServletRequest req) throws IOException, HttpException {
 
 		String code = req.getParameter("code");
 		StringBuffer sb = postit(code,req.getRequestURL().toString());
-		
+
 		JSONObject jObject;
 		String accessToken = null;
 		try {
 			jObject = new JSONObject(sb.toString());
 			accessToken = jObject.getString("access_token");
-			
+
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			log.debug("Error In method exchangeCodeForAccessToken: " + e.toString());
@@ -124,62 +123,56 @@ public class PersonalApi {
 		//log.debug("req url: "+req.getRequestURL().toString());
 		return accessToken;
 	}
-	
-	public String getit(String url, String aToken, String secure_pass){
+
+	public String getit(String url, String aToken, String secure_pass) throws IOException {
 		//String url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=xrb8fprukkstk5g94v26jhuz";
 		String charset = "UTF-8";
 		String result = null;
-		try 
+
+		URLConnection conn = new URL(url).openConnection();
+
+		conn.setRequestProperty("Authorization", "Bearer " + aToken);
+		log.debug("Headers: " + conn.getHeaderFields().toString());
+
+		//System.out.println("get req prop"+conn.getRequestProperties().toString());
+		InputStream response = conn.getInputStream();
+		// GET request starts
+		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		StringBuffer sb = new StringBuffer();
+		String line;
+		while ((line = rd.readLine()) != null)
 		{
-			URLConnection conn = new URL(url).openConnection();
-			
-			conn.setRequestProperty("Authorization", "Bearer " + aToken);
-			
-			System.out.println("get header fields"+conn.getHeaderFields().toString());
-			//System.out.println("get req prop"+conn.getRequestProperties().toString());
-			InputStream response = conn.getInputStream();
-			// GET request starts
-			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			StringBuffer sb = new StringBuffer();
-			String line;
-			while ((line = rd.readLine()) != null)
-			{
-				sb.append(line);
-			}
-			rd.close();
-			result = sb.toString();
-		} 
-		catch (Exception e)
-		{
-			System.out.println("Error: " + e.toString());
+			sb.append(line);
 		}
-		
+		rd.close();
+		result = sb.toString();
+
 		return result;
 	}
 
 	public JSONObject getUser(String accessToken) throws IOException, JSONException {
 
 		if (accessToken == null) throw new NullPointerException();
-		
+
 		log.debug("Retrieving User for Access Token '" + accessToken + "'");
 
 		String url = "https://api-sandbox.personal.com/api/v1/gems/?client_id="+this.appId;
 		String res = getit(url,accessToken,null);
-		
+
 		String instanceID = null;
-		
+
 		List<String> encInstanceId = new ArrayList<String>();
 		try {
 			JSONObject jj = new JSONObject(res);
 			org.json.JSONArray gemContents = jj.getJSONArray("gems");
 
 			instanceID = gemContents.getJSONObject(0).getString("gem_instance_id");
-			
+
 			//@SuppressWarnings("deprecation")
 			//encInstanceId = URLEncoder.encode(gemContents.getJSONObject(0).getString("gem_instance_id"));
-			encInstanceId.add(URLEncoder.encode(gemContents.getJSONObject(0).getString("gem_instance_id")));
-			encInstanceId.add(URLEncoder.encode(gemContents.getJSONObject(1).getString("gem_instance_id")));
-			
+			encInstanceId.add(URLEncoder.encode(gemContents.getJSONObject(0).getString("gem_instance_id"), "UTF-8"));
+			encInstanceId.add(URLEncoder.encode(gemContents.getJSONObject(1).getString("gem_instance_id"), "UTF-8"));
+
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -192,7 +185,7 @@ public class PersonalApi {
 			url = "https://api-sandbox.personal.com/api/v1/gems/"+i+"/?client_id="+this.appId;
 			String secure_pass = this.appSecret;
 			gemData = getit(url,accessToken,secure_pass);
-			
+			log.info(gemData);
 			try {
 				JSONObject tempObj = new JSONObject(gemData).getJSONObject("gem").getJSONObject("data");
 				//nameGemObject = tempObj;
@@ -201,8 +194,8 @@ public class PersonalApi {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
-			
-			
+
+
 		}
 
 		log.debug("User: " + gemObject);
@@ -233,7 +226,7 @@ public class PersonalApi {
 
 		this.appSecret = appSecret;
 	}
-	
+
 	public String getScope() {
 
 		return this.scope;
@@ -243,7 +236,7 @@ public class PersonalApi {
 
 		this.scope = appSecret;
 	}
-	
+
 	public String getUpdate() {
 
 		return this.update;
