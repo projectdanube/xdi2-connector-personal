3b8b902e629757839c7ff2c2c913dd78dbb04bb5
peacekeeper <markus.sabadello@gmail.com>
2012-10-09 19:00:41 +0200

    fix applicationContext.xml

--- src/main/java/xdi2/connector/personal/contributor/PersonalContributor.java
+++ src/main/java/xdi2/connector/personal/contributor/PersonalContributor.java
@@ -18,14 +18,16 @@ import xdi2.messaging.MessageEnvelope;
 import xdi2.messaging.MessageResult;
 import xdi2.messaging.exceptions.Xdi2MessagingException;
 import xdi2.messaging.target.ExecutionContext;
+import xdi2.messaging.target.MessagingTarget;
 import xdi2.messaging.target.Prototype;
 import xdi2.messaging.target.contributor.AbstractContributor;
 import xdi2.messaging.target.contributor.ContributorXri;
 import xdi2.messaging.target.impl.graph.GraphMessagingTarget;
 import xdi2.messaging.target.interceptor.MessageEnvelopeInterceptor;
+import xdi2.messaging.target.interceptor.MessagingTargetInterceptor;
 
 @ContributorXri(addresses={"(https://personal.com)"})
-public class PersonalContributor extends AbstractContributor implements MessageEnvelopeInterceptor, Prototype<PersonalContributor> {
+public class PersonalContributor extends AbstractContributor implements MessagingTargetInterceptor, MessageEnvelopeInterceptor, Prototype<PersonalContributor> {
 
 	private static final Logger log = LoggerFactory.getLogger(PersonalContributor.class);
 
@@ -51,13 +53,6 @@ public class PersonalContributor extends AbstractContributor implements MessageE
 
 		PersonalContributor contributor = new PersonalContributor();
 
-		// set the token graph
-
-		if (this.tokenGraph == null && prototypingContext.getMessagingTarget() instanceof GraphMessagingTarget) {
-
-			contributor.setTokenGraph(((GraphMessagingTarget) prototypingContext.getMessagingTarget()).getGraph());
-		}
-
 		// set api and mapping
 
 		contributor.setPersonalApi(this.getPersonalApi());
@@ -66,6 +61,26 @@ public class PersonalContributor extends AbstractContributor implements MessageE
 		// done
 
 		return contributor;
+	}
+
+	/*
+	 * MessagingTargetInterceptor
+	 */
+
+	@Override
+	public void init(MessagingTarget messagingTarget) throws Exception {
+
+		// set the token graph
+
+		if (this.tokenGraph == null && messagingTarget instanceof GraphMessagingTarget) {
+
+			this.setTokenGraph(((GraphMessagingTarget) messagingTarget).getGraph());
+		}
+	}
+
+	@Override
+	public void shutdown(MessagingTarget messagingTarget) throws Exception {
+
 	}
 
 	/*
